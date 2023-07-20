8f960bd299a1b6181d6600b93d0996be3f41c48c
Sukanta Das <arnobh84@gmail.com>
2023-07-20 19:14:58 +0600

    search result page created

--- src/router/routes.jsx
+++ src/router/routes.jsx
@@ -1,11 +1,18 @@
 import { createBrowserRouter } from "react-router-dom";
 import MainLayout from "../Layout/MainLayout";
 import Feed from "../pages/feed/feed";
+import SearchResult from "../pages/search-Result/search-result";
 
 export const router = createBrowserRouter([
   {
     path: "/",
     element: <MainLayout />,
-    children: [{ path: "/", element: <Feed /> }],
+    children: [
+      { path: "/", element: <Feed /> },
+      {
+        path: "/searchResult/:searchQuery",
+        element: <SearchResult />,
+      },
+    ],
   },
 ]);
